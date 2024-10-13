e21ed47aea8e405578c0efb8aeb01f05b772ee80
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-10-14 01:08:45 +0700

    menambahkan conditional expression when di dalam function

--- app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
+++ app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
@@ -10,5 +10,16 @@ fun ConditionalStatement() {
         println("Negative number")
     }
 
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid day")
 
+    }
 }
