07f1931ae08644760c69362d43816b3e58a11225
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-10-14 01:35:53 +0700

    2 fungsi dengan parameter default dan tidak ada defaultnya

--- app/src/main/java/com/example/praktikum1/Functions.kt
+++ app/src/main/java/com/example/praktikum1/Functions.kt
@@ -9,3 +9,24 @@ fun withParameter(name: String) {
     println("== withParameter ==")
     println("Hello $name")
 }
+
+fun withNamedParameter(Name: String, age: Int) {
+    println("== withNamedParameter ==")
+    println("Hello $Name, your $age years old")
+}
+
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println("== withDefaultParameter ==")
+    println("Hello $name, your $age years old")
+}
+
+fun withReturn(panjang: Int, lebar: Int): Int {
+    return panjang * lebar
+}
+
+fun main(){
+    withoutParameter()
+    withParameter("John")
+    withNamedParameter(age = 30, Name = "John")
+    withDefaultParameter(age = 25)
+}
