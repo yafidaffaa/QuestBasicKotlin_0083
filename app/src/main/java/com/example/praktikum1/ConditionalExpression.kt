5d0938b926cd55b2ed611c59397e23f09fc3f65e
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-10-14 01:18:08 +0700

    membuat jenis perulangan dengan while dan do while

--- app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
+++ app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
@@ -46,5 +46,22 @@ fun Perulangan() {
         println("Car type: $car")
     }
 
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main() {
+    ConditionalStatement()
+    Perulangan()
 
 }
