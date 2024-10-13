19540aecfbe83947e27b97a8b9a0b038b3f5b0a3
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-10-14 01:27:50 +0700

    membuat variable untuk input kemudian mencetaknya dengan salah satunya menggunakan fungsi equal

--- app/src/main/java/com/example/praktikum1/DataClass.kt
+++ app/src/main/java/com/example/praktikum1/DataClass.kt
@@ -5,3 +5,17 @@ data class  DataClass(
     val email: String
 )
 
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2)
+
+    val data3 = data.copy()
+    println(data3)
+
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+
+}
