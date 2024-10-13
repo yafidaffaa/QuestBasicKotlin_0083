deffb48fd83df1a4d403ca817e5554603479fb28
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-10-14 01:06:19 +0700

    membuat sebuah function dengan conditional expression if else

---
+++ app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum1
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    val number = 10
+    if (number > 0) {
+        println("Positive number")
+    } else {
+        println("Negative number")
+    }
+
+
+}
