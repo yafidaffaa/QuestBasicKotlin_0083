3890b8e7a60db60d000b100959fb145dd1ea8b7a
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-10-14 01:32:45 +0700

    membuat 2 function dengan parameter dan tidak

---
+++ app/src/main/java/com/example/praktikum1/Functions.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum1
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello World")
+}
+
+fun withParameter(name: String) {
+    println("== withParameter ==")
+    println("Hello $name")
+}
